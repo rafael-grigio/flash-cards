f95da72b0f6946e2bd611b6f0dc84b63bab26d12
Samuel1-27 <167532776+Samuel1-27@users.noreply.github.com>
2024-11-20 12:38:32 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-
+function criaCartao (categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'  
+    cartao.innerHTML `<div class="cartao__conteudo">
+                 <h3>${categoria}</h3>
+                 <div class="cartao__conteudo__pergunta">
+                     <p>${pergunta}</p>
+                 </div>
+                 <div class="cartao__conteudo__resposta">
+                     <p>${resposta}</p>
+                 </div>
+    `
+    let respostaEstaVisivel = false
+ 
+    function viraCartao(){
+     respostaestaVisivel = !respostaEstaVisivel
+     cartao.classList.toggle('active', respostaEstaVisivel)
+ }
+ cartao.addEventListener('click', viraCartao)
+ 
+ 
+    container.appendChild(cartao)
+ }
