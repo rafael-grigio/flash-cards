efb4a8f9f320f12f0eacc169d3f208b732d5919e
Samuel1-27 <167532776+Samuel1-27@users.noreply.github.com>
2024-11-20 12:39:09 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,28 @@
+criaCartao(
+    'Matemática',
+    'Na matemática, o que utilizamos para descobrir o cateto da hipotenusa?',
+    'O Teorema de Pitágoras'
+)
 
+criaCartao(
+    'Portugês',
+    'O que é um adjetivo?',
+    'É a característica de um indivíduo.'
+)
+
+criaCartao(
+    'Música',
+    'Quantas notas há em uma tetrade?',
+    'Quatro notas (Tônica, terça maior, quinta justa e a 7M)'
+)
+criaCartao(
+    'Conhecimentos Gerais',
+    'Quantos minutos tem um dia?',
+    '1440 minutos.'
+)
+
+criaCartao(
+    'Química',
+    'Na química orgânica, que molécula um material é preciso ter para ser considerado orgânico?',
+    'Uma molécula de carbono.'
+)
